43dba919def618551279cf9438feff22b4a54ad9
DanielSimek548 <73018736+DanielSimek548@users.noreply.github.com>
2022-03-29 10:32:44 +0200

    Add files via upload

--- action_page.php
+++ action_page.php
@@ -9,33 +9,9 @@
         <title>Document</title>
     </head>
     <body>
-        <header>
-            <nav class="navbar navbar-dark bg-dark">
-                <div class="container-fluid">
-                    <a class="navbar-brand">MY PAGE</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                        <ul class="navbar-nav">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="action_page.php">Home</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="register.php">Sign up</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="login.php">Sign in</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div> 
-            </nav>
-        </header>
+        <?php include('./parts/header.php');?>
         
-        <footer>
-            
-        </footer>
+        <?php include('./parts/footer.php');?>
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     </body>
--- login.php
+++ login.php
@@ -9,9 +9,13 @@ Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/html.html to edit this
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <style>
-            body {font-family: Arial, Helvetica, sans-serif;}
-            form {border: 3px solid #f1f1f1;}
-            
+            body {
+                font-family: Arial, Helvetica, sans-serif;
+            }
+            form {
+                border: 3px solid #f1f1f1;
+            }
+
             input[type=text], input[type=password]{
                 width: 100%;
                 padding: 12px 20px;
@@ -20,7 +24,7 @@ Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/html.html to edit this
                 border: 1px solid #ccc;
                 box-sizing: border-box;
             }
-            
+
             button{
                 background-color: #04AA6D;
                 color: white;
@@ -30,76 +34,81 @@ Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/html.html to edit this
                 cursor: pointer;
                 width: 100%;
             }
-            
+
             button:hover{
                 opacity: 0.8;
             }
-            
+
             .cancelbtn{
                 width: auto;
                 padding: 10px 18px;
                 background-color: #f44336;
             }
-            
+
             .container{
                 padding: 16px;
             }
-            
+
             span.reg{
                 float: right;
                 padding-top: 16px;
             }
-            
+
             span.psw{
                 float: right;
                 padding-top: 16px;
             }
-            
+
             @media screen and (max-width: 300px){
                 span.reg{
                     display: block;
                     float: none;
                 }
-                
+
                 span.psw{
                     display: block;
                     float: none;
                 }
-                
+
                 .cancelbtn{
                     width: 100%;
                 }
-            } 
+            }
         </style>
     </head>
-    
+
     <body>
-        <?php include('./parts/header.php');?>
+        <?php include('./parts/header.php'); ?>
         <p></p>
-        
+
         <h2>Login</h2>
-        
+
         <form action="script/log_script.php" method="post">
             <div class="container">
                 <label for="uname"><b>Username</b></label>
                 <input type="text" placeholder="Enter Username" name="uname" required>
-                
+
                 <label for="pasw"><b>Password</b></label>
                 <input type="password" placeholder="Enter Password" name="psw" required>
-                
+
                 <button type="submit">Login</button>
                 <label>
                     <input type="checkbox" checked="checked" name="remember">Remember me
                 </label>
             </div>
-            
+
             <div class="container" style="background-color:#f1f1f1">
                 <button type="button" class="cancelbtn">Cancel</button>
                 <span class="reg">Do not have an account? <a href="register.php">Register</a>.</span>
             </div>
-                
+
+            <div>
+                <p><?php if(isset($_GET['msg'])) echo $_GET['msg']?></p>
+            </div>
+            
         </form>
-        <?php include('./parts/footer.php');?>
+        
+        <?php include('./parts/footer.php'); ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     </body>
 </html>
